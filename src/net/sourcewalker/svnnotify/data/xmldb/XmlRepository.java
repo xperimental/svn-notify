6d10d33ee928e3ac3d3f646e5f775a6c2e2dc3b1
Robert <xperimental@solidproject.de>
2009-10-13 18:33:47 +0200

    Implemented update routine in XmlRepository

--- src/net/sourcewalker/svnnotify/data/xmldb/XmlRepository.java
+++ src/net/sourcewalker/svnnotify/data/xmldb/XmlRepository.java
@@ -9,21 +9,20 @@ import net.sourcewalker.svnnotify.data.interfaces.IRevision;
 
 /**
  * @author Xperimental
- *
+ * 
  */
 public class XmlRepository implements IRepository {
 
 	String name;
 	URI url;
 	List<IRevision> revisions;
-	
-	public XmlRepository(String name, URI url)
-	{
+
+	public XmlRepository(String name, URI url) {
 		this.name = name;
 		this.url = url;
 		revisions = new ArrayList<IRevision>();
 	}
-	
+
 	@Override
 	public List<IRevision> getAllRevisions() {
 		return revisions;
@@ -32,8 +31,7 @@ public class XmlRepository implements IRepository {
 	@Override
 	public int getLastRevisionNumber() {
 		int maxRev = 0;
-		for (IRevision r: revisions)
-		{
+		for (IRevision r : revisions) {
 			if (r.getRevision() > maxRev)
 				maxRev = r.getRevision();
 		}
@@ -48,10 +46,8 @@ public class XmlRepository implements IRepository {
 	@Override
 	public IRevision getRevision(int revNumber) {
 		IRevision result = null;
-		for (IRevision r: revisions)
-		{
-			if (r.getRevision() == revNumber)
-			{
+		for (IRevision r : revisions) {
+			if (r.getRevision() == revNumber) {
 				result = r;
 				break;
 			}
@@ -66,8 +62,31 @@ public class XmlRepository implements IRepository {
 
 	@Override
 	public List<IRevision> updateRepository(List<IRevision> update) {
-		// TODO Auto-generated method stub
-		return null;
+		List<IRevision> updatedRevisions = new ArrayList<IRevision>();
+
+		for (IRevision newRev : update) {
+			IRevision repoRev = getRevision(newRev.getRevision());
+			if (repoRev != null) {
+				boolean changed = false;
+				if (!repoRev.getAuthor().equals(newRev.getAuthor()))
+					changed = true;
+				if (!repoRev.getTimestamp().equals(newRev.getTimestamp()))
+					changed = true;
+				if (!repoRev.getMessage().equals(newRev.getMessage()))
+					changed = true;
+				if (changed)
+				{
+					revisions.remove(repoRev);
+					revisions.add(newRev);
+					updatedRevisions.add(newRev);
+				}
+			} else {
+				revisions.add(newRev);
+				updatedRevisions.add(newRev);
+			}
+		}
+
+		return updatedRevisions;
 	}
 
 }
