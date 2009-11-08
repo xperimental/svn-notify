99910710e2c0a0641dfe03881c1fcf7e8d73f2d2
Robert <xperimental@solidproject.de>
2009-11-08 18:29:29 +0100

    Added Javadoc for INotifier.

--- src/net/sourcewalker/svnnotify/data/interfaces/INotifier.java
+++ src/net/sourcewalker/svnnotify/data/interfaces/INotifier.java
@@ -3,10 +3,23 @@ package net.sourcewalker.svnnotify.data.interfaces;
 import java.util.List;
 
 /**
+ * Classes implementing this interface are responsible for notifying the user of
+ * new revisions. How the user is notified is subject to the class
+ * implementation.
+ *
  * @author Xperimental
  */
 public interface INotifier {
 
+    /**
+     * Notify the user about new revisions in a repository. This method is
+     * called once for each repository with new revisions.
+     *
+     * @param repository
+     *            Repository with new revisions.
+     * @param revisions
+     *            List with all new revisions.
+     */
     void reportUpdates(IRepository repository, List<IRevision> revisions);
 
 }
