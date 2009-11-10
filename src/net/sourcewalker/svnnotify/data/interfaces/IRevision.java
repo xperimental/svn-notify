3dc346383859fddde6169a1759015ea4900b99f9
Robert <xperimental@solidproject.de>
2009-11-10 21:39:15 +0100

    Added Javadoc for IRevision.

--- src/net/sourcewalker/svnnotify/data/interfaces/IRevision.java
+++ src/net/sourcewalker/svnnotify/data/interfaces/IRevision.java
@@ -3,16 +3,39 @@ package net.sourcewalker.svnnotify.data.interfaces;
 import java.util.Date;
 
 /**
+ * Classes implementing this interface are used to save information about a
+ * revision contained in a repository.
+ *
  * @author Xperimental
  */
 public interface IRevision {
 
+    /**
+     * Returns the revision number of this revision.
+     *
+     * @return Revision number.
+     */
     int getRevision();
 
+    /**
+     * Returns the author of this revision.
+     *
+     * @return Author of revision.
+     */
     String getAuthor();
 
+    /**
+     * Returns the time this revision was created.
+     *
+     * @return Creation time of revision.
+     */
     Date getTimestamp();
 
+    /**
+     * Returns the commit message (comment) of this revision.
+     *
+     * @return Commit message of revision.
+     */
     String getMessage();
 
 }
