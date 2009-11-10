3bbdd4a1e0d2e2d8d2a10a902c43d0ea525fe5c8
Robert <xperimental@solidproject.de>
2009-11-10 21:28:42 +0100

    Added Javadoc for IProvider.

--- src/net/sourcewalker/svnnotify/data/interfaces/IProvider.java
+++ src/net/sourcewalker/svnnotify/data/interfaces/IProvider.java
@@ -3,13 +3,38 @@ package net.sourcewalker.svnnotify.data.interfaces;
 import java.util.List;
 
 /**
+ * Classes implementing this interface provide the ability to query repositories
+ * for new revisions by connecting to the servers.
+ *
  * @author Xperimental
  */
 public interface IProvider {
 
+    /**
+     * Gets all revisions from the specified repository.
+     *
+     * @param factory
+     *            Object factory which should be used to create new revision
+     *            objects.
+     * @param repository
+     *            Repository to query for revisions.
+     * @return List of all revisions in the repository.
+     */
     List<IRevision> getAllRevisions(IObjectFactory factory,
             IRepository repository);
 
+    /**
+     * Gets new revisions from the repository server. The highest revision
+     * number saved in the repository object is used to determine which
+     * revisions are new.
+     *
+     * @param factory
+     *            Object factory which should be used to create new revision
+     *            objects.
+     * @param repository
+     *            Repository to query for new revisions.
+     * @return List of new revisions since last query.
+     */
     List<IRevision> getNewRevisions(IObjectFactory factory,
             IRepository repository);
 
