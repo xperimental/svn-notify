2ae76526af0b861d97cab40045fac8fd44909784
Robert <xperimental@solidproject.de>
2009-11-10 23:07:22 +0100

    Fixed checkstyle warnings.

--- src/net/sourcewalker/svnnotify/notifier/NullNotifier.java
+++ src/net/sourcewalker/svnnotify/notifier/NullNotifier.java
@@ -7,12 +7,17 @@ import net.sourcewalker.svnnotify.data.interfaces.IRepository;
 import net.sourcewalker.svnnotify.data.interfaces.IRevision;
 
 /**
+ * This class contains a notifier implementation which simply does nothing. It
+ * is intended to be used in the early development phase or in case the
+ * repository information should only be updated but not displayed to the user.
+ *
  * @author Xperimental
  */
 public class NullNotifier implements INotifier {
 
     @Override
-    public void reportUpdates(IRepository repository, List<IRevision> revisions) {
+    public void reportUpdates(final IRepository repository,
+            final List<IRevision> revisions) {
         // Do nothing.
     }
 
